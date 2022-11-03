15cb2472f2aea2c75cf3babcce9fffc958ae516d
DrexHD <nicknamedrex@gmail.com>
2022-11-04 00:30:31 +0100

    update: 22w44a

--- src/main/java/eu/pb4/stylednicknames/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/eu/pb4/stylednicknames/mixin/ServerPlayerEntityMixin.java
@@ -20,8 +20,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity {
-    public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile, @Nullable PlayerPublicKey publicKey) {
-        super(world, pos, yaw, gameProfile, publicKey);
+    public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
+        super(world, pos, yaw, gameProfile);
     }
 
     @Inject(method = "getPlayerListName", at = @At("TAIL"), cancellable = true)
