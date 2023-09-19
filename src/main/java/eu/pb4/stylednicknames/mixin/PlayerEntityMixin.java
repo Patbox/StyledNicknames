d5be34fe8cab6a8645c690d69b8bb8a8332494bf
Patbox <39821509+Patbox@users.noreply.github.com>
2023-09-19 17:11:18 +0200

    Update to 1.20.2

--- src/main/java/eu/pb4/stylednicknames/mixin/PlayerEntityMixin.java
+++ src/main/java/eu/pb4/stylednicknames/mixin/PlayerEntityMixin.java
@@ -2,8 +2,11 @@ package eu.pb4.stylednicknames.mixin;
 
 import eu.pb4.stylednicknames.NicknameHolder;
 import eu.pb4.stylednicknames.config.ConfigManager;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,13 +14,23 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 
 @Mixin(PlayerEntity.class)
-public abstract class PlayerEntityMixin {
-    @Unique boolean styledNicknames$ignoreNextCall = false;
+public abstract class PlayerEntityMixin extends LivingEntity {
+    @Unique private boolean styledNicknames$ignoreNextCall = false;
+    @Unique private Text styledNicknames$cachedName = null;
+    @Unique private int styledNicknames$cachedAge = -999;
+
+    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
 
     @ModifyArg(method = "getDisplayName", at = @At(value = "INVOKE", target = "Lnet/minecraft/scoreboard/Team;decorateName(Lnet/minecraft/scoreboard/AbstractTeam;Lnet/minecraft/text/Text;)Lnet/minecraft/text/MutableText;"))
     private Text styledNicknames$replaceName(Text text) {
         try {
             if (ConfigManager.isEnabled() && ConfigManager.getConfig().configData.changeDisplayName) {
+                if (this.styledNicknames$cachedAge == this.age) {
+                    return this.styledNicknames$cachedName;
+                }
+
                 if (!this.styledNicknames$ignoreNextCall) {
                     this.styledNicknames$ignoreNextCall = true;
                     var holder = NicknameHolder.of(this);
@@ -25,13 +38,15 @@ public abstract class PlayerEntityMixin {
                         Text name = holder.styledNicknames$getOutput();
                         if (name != null) {
                             this.styledNicknames$ignoreNextCall = false;
+                            this.styledNicknames$cachedName = name;
+                            this.styledNicknames$cachedAge = this.age;
                             return name;
                         }
                     }
                     this.styledNicknames$ignoreNextCall = false;
                 }
             }
-        } catch (Exception e) {
+        } catch (Throwable e) {
             e.printStackTrace();
         }
         return text;
--- src/main/java/eu/pb4/stylednicknames/mixin/PlayerManagerMixin.java
+++ src/main/java/eu/pb4/stylednicknames/mixin/PlayerManagerMixin.java
@@ -3,6 +3,7 @@ package eu.pb4.stylednicknames.mixin;
 import eu.pb4.stylednicknames.NicknameHolder;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ConnectedClientData;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PlayerManager.class)
 public class PlayerManagerMixin {
     @Inject(method = "onPlayerConnect", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/PlayerManager;sendCommandTree(Lnet/minecraft/server/network/ServerPlayerEntity;)V", shift = At.Shift.AFTER))
-    private void styledNicknames$loadNickname(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
+    private void styledNicknames$loadNickname(ClientConnection connection, ServerPlayerEntity player, ConnectedClientData clientData, CallbackInfo ci) {
         NicknameHolder.of(player).styledNicknames$loadData();
     }
 }
